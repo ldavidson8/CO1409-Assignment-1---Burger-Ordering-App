1137127eefd0e594c288e3c957eaa843ad1580bc
lewis davidson <lewisdavidson8@gmail.com>
2023-01-25 18:09:32 +0000

    Cleanup, moved struct of toppings to outside function. TODO Checkout with total price

--- CO1409 Assignment 1 - Burger Ordering App/Source.cpp
+++ CO1409 Assignment 1 - Burger Ordering App/Source.cpp
@@ -22,15 +22,25 @@ struct Topping {
 	double price;
 };
 
+// Structure of the available toppings
+const Topping toppings[NUM_TOPPINGS] = {
+	{"Cheese", 0.8},
+	{"Salad", 0.5},
+	{"Bacon", 1.0},
+	{"Ketchup", 0.3},
+	{"Mayonnaise", 0.3},
+	{"Extra patty", 1.5}
+};
+
 
 // Function Declarations
 int ShowMenu(double &balance);
 void AddCredits(double &balance);
 void ShowBurgerSizes(double &balance);
 void SelectBurgerSize(double &balance);
-void ShowToppings();
+void ShowToppings(double &price, string &burgerSize);
 void SelectToppings(double &balance);
-double CalcRequiredCredits(double &balance);
+void Checkout(double &balance);
 
 
 int main()
@@ -65,7 +75,6 @@ int ShowMenu(double &balance)
 		cout << "Sorry, please select a valid option from the menu" << '\n';
 		ShowMenu(balance);
 	}
-
 	switch (menuChoice)
 	{
 	case 0:
@@ -84,6 +93,7 @@ int ShowMenu(double &balance)
 		SelectBurgerSize(balance);
 		break;
 	default:
+		return 0;
 		break;
 	}
 }
@@ -120,8 +130,9 @@ void SelectBurgerSize(double &balance)
 	system("cls");
 	ShowBurgerSizes(balance);
 	string burgerSize;
+	double price{};
 	int choice;
-	while (!(cin >> choice) || choice < 1 || choice > 4)
+	while (!(cin >> choice) || choice < 0 || choice > 3)
 	{
 		ClearInputBuffer();
 		cout << "Sorry, please select a valid option from the menu" << '\n';
@@ -133,16 +144,19 @@ void SelectBurgerSize(double &balance)
 		break;
 	case 1:
 		burgerSize = "Small";
+		price += 3.0;
 		cout << "You selected a " << burgerSize << " burger." << '\n';
 		SelectToppings(balance);
 		break;
 	case 2:
 		burgerSize = "Medium";
+		price += 5.5;
 		cout << "You selected a " << burgerSize << " burger." << '\n';
 		SelectToppings(balance);
 		break;
 	case 3:
 		burgerSize = "Large";
+		price += 7.25;
 		cout << "You selected a " << burgerSize << " burger." << '\n';
 		SelectToppings(balance);
 		break;
@@ -155,30 +169,46 @@ void SelectBurgerSize(double &balance)
 
 void ShowToppings()
 {
-	Topping toppings[NUM_TOPPINGS] = {
-		{"Cheese", 0.8},
-		{"Salad", 0.5},
-		{"Bacon", 1.0},
-		{"Ketchup", 0.3},
-		{"Mayo", 0.3},
-		{"Extra patty", 1.5}
-	};
-	 
 	cout << "Select your toppings:" << '\n';
 
 	for (int i = 0; i < NUM_TOPPINGS; i++)
 	{
 
-		cout << i + 1 << ". " << toppings[i].name << '\t' << toppings[i].price << ".\n";
+		cout << i + 1 << ". " << toppings[i].name << '\t' << toppings[i].price << '\n';
 	}
+	cout << "7. Checkout" << '\n';
+	cout << "Please enter a number: ";
 }
 
 void SelectToppings(double &balance)
 {
+	vector<string> selectedToppings;
 	ShowToppings();
+	int choice;
+	do {
+		while (!(cin >> choice) && choice < NUM_TOPPINGS && choice > NUM_TOPPINGS + 1)
+		{
+			ClearInputBuffer();
+			cout << "Sorry, please select an option from the menu\n";
+		}
+		switch (choice)
+		{
+		case 1: case 2: case 3: case 4: case 5: case 6:
+			selectedToppings.push_back(toppings[choice - 1].name); // Select topping from array using user input - 1 
+			cout << "You've added " << toppings[choice - 1].name << " to your order.\n";
+			break;
+		case 7:
+			Checkout(balance);
+			break;
+		default:
+			cout << "Invalid selection. Please enter a number listed from the menu" << '\n';
+			SelectBurgerSize(balance);
+			break;
+		}
+	} while (choice != 7);
 }
 
-double CalcRequiredCredits(double &balance)
+void Checkout(double &balance)
 {
-	return 0;
+	cout << "Test from checkout function" << '\n';
 }
